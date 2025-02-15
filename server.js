8f3363545d07f95268732cd216bec7175a33a18c
charmainelow1 <125853983+charmainelow1@users.noreply.github.com>
2025-02-15 16:37:13 +0800

    Logger Middleware

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import path from 'path';
 import users from './routes/users.js'
+import logger from './middleware/logger.js'
 const port = process.env.PORT || 8000;
 
 const app = express();
 
 //body parser middleware
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+//logger middleware
+app.use(logger);
 
 app.use('/', users);
 
